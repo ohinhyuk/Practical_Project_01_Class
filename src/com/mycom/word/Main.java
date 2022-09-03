7efbc981cf68a58b0a277e27673ef995427c8656
ohinhyuk <8156217@naver.com>
2022-09-03 15:07:57 +0900

    Project 1 Project Configuration

---
+++ src/com/mycom/word/Main.java
@@ -0,0 +1,12 @@
+package com.mycom.word;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new WordManager().start();
+		System.out.println("hello");
+	}
+	
+	
+}
